8b11a0c682a89832564cc557de85385148e80382
mil0h <155496060+mil0h@users.noreply.github.com>
2024-01-18 21:36:25 +0000

    a bunch of stuff

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+COMMENTS = [
+    {
+        'username': 'Jack Robinson',
+        'text': 'I love this app!',
+        'likes': '0'
+    },
+    {
+        'username':
+        'Gbenga Sobowale',
+        'age':
+        '41',
+        'text':
+        'My daughter uses the website and has started to really enjoy maths. Props to the devs!'
+    },
+    {
+        'username':
+        'Scarlet Smith',
+        'age':
+        '9',
+        'text':
+        'Because of this website, I can now do addition and subtraction. I am so happy!'
+    },
+    {
+        'username':
+        'Snow White',
+        'age':
+        '18',
+        'text':
+        'The dwarves have loved using the fun and interactive website. I am glad I discovered it!'
+    },
+]
+
+
 @app.route("/")
 def hello_world():
-  return "<h1>Maths Made Easy</h1>"
+  return render_template('home.html', comments=COMMENTS)
+
 
 if __name__ == "__main__":
-  app.run(host= "0.0.0.0", debug=True)
+  app.run(host="0.0.0.0", debug=True)
+
