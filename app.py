af81a090b94cef54e415f2783aaefa0d00a97f4a
mil0h <155496060+mil0h@users.noreply.github.com>
2024-01-30 00:30:55 +0000

    fixed navbar and added dynamic data

--- app.py
+++ app.py
@@ -39,6 +39,10 @@ COMMENTS = [
 def hello_world():
   return render_template('home.html', comments=COMMENTS)
 
+@app.route("/level1")
+def level1stuff():
+  return render_template('level1.html')
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
